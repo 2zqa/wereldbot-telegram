17dd9a12f75e07fe440aa2496d6f3fdea5c49de7
2zqa <25235249+2zqa@users.noreply.github.com>
2021-05-03 17:48:25 +0200

    Add project_setup.py

--- setup_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -p 'Enter your telegram bot token: ' BOT_TOKEN
-read -p 'Enter webhook url: ' WEBHOOK_URL
-
-echo 'TELEGRAM_BOT_TOKEN="'$BOT_TOKEN'"' > .env
-echo 'TELEGRAM_WEBHOOK="'$WEBHOOK_URL'"' >> .env
-echo Written info to .env
